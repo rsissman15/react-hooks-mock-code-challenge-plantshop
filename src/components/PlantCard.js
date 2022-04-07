f838724e2c5e2bcb9ae3eb264b74b37f84bb17eb
Rebecca Sissman <rsissman15@aol.com>
2022-04-07 14:39:29 -0400

    Completed Assigment

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+  const [inStock,setStock]=useState(true)
+
+  function handleClick(){
+    setStock(!inStock)
+  }
+
+  const handleButtonClass= inStock ? "primary" : ""
+  const handleButtonName= inStock ? "In Stock" : "Out of Stock"
+
+
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      <button onClick={handleClick} className={handleButtonClass}>{handleButtonName}</button>
+
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants,setPlant]=useState([])
+  const [search, setSearch] = useState('')
+
+
+  const filteredPlants = plants.filter(plant => plant.name.toLowerCase().includes(search.toLowerCase()))
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/plants")
+    .then(res=>res.json())
+    .then(plantData=>setPlant(plantData))
+  },[])
+
+
+  function addPlant(plant){
+    setPlant([...plants,plant])
+    
+  }
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant}/>
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
